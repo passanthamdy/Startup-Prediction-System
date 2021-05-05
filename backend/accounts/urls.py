51797d9cdbf60fd34a8a9e84ad974cd91a0d73ac
AhmadAmr <ahmedamr18841@gmail.com>
2021-05-05 05:37:02 +0200

    CREATE USER REGISTRATION WITH JWT TOKEN AUTH

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-from .views import ObtainTokenPairWithColorView
+from .views import ObtainTokenPairWithColorView ,CreateCustomUser,HelloWorldView
 
 urlpatterns = [
+    path('user/create/', CreateCustomUser.as_view(), name="create_user"),
     path('token/obtain/', ObtainTokenPairWithColorView.as_view(), name='token_create'),  
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('hello/', HelloWorldView.as_view(), name='hello_world'),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView 
+from .serializers import CustomUserSerializer
+from rest_framework import permissions,status
+from rest_framework.response import Response
 from .serializers import MyTokenObtainPairSerializer
+from rest_framework.views import APIView
+
 # Create your views here.
 
 class ObtainTokenPairWithColorView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = MyTokenObtainPairSerializer
+
+
+class CreateCustomUser(APIView):
+    permission_classes=(permissions.AllowAny ,)
+
+    def post(self,request ,format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json_obj = serializer.data
+                return Response(json_obj, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class HelloWorldView(APIView):
+
+    def get(self, request):
+        return Response(data={"hello":"world"}, status=status.HTTP_200_OK)
