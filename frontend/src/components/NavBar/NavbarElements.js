6425469faf893187cb5004405837e075ae90d799
yusseef <yussef.raouf11@gmail.com>
2021-05-10 03:03:46 +0200

    make navbar transparent

--- frontend/src/components/NavBar/NavbarElements.js
+++ frontend/src/components/NavBar/NavbarElements.js
@@ -3,9 +3,9 @@ import { Link as LinkR } from 'react-router-dom'
 import { Link as LinkS } from 'react-scroll'
 
 export const Nav = styled.nav`
-background:#000;
-height: 80px;
-//margin-top:-80px;
+background:${({scrollNav}) => (scrollNav ? '#000' : 'transparent')};
+height: 100px;
+margin-top:-100px;
 display:flex;
 justify-content: center;
 align-items: center;
--- frontend/src/components/NavBar/index.js
+++ frontend/src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {FaBars} from 'react-icons/fa'
 import logo from '../../images/logo.png'
 import {
@@ -18,9 +18,22 @@ import {
 
 
 const Navbar = ({toggle}) => {
+    const [scrollNav, setScrollNav] = useState(false);
+
+    const changeNav = () => {
+        if(window.scrollY >= 80){
+            setScrollNav(true)
+        } else{
+            setScrollNav(false)
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', changeNav)
+    }, [])
+
     return (
         <>
-        <Nav>
+        <Nav scrollNav={scrollNav}>
             <NavbarContainer>
                 <NavLogo to='/'><img src={logo} alt="logo"></img></NavLogo>
                 <MobileIcon onClick= {toggle}>
