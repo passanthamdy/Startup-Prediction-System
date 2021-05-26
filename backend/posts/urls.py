1a7f478303837b9a94188c72945a72d2577fb540
Ahmadamr <ahmedamr18841@gmail.com>
2021-05-27 00:07:22 +0200

    change url

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -2,8 +2,8 @@ from django.urls import  path
 from .views import PostListView,PostDetailView, CategoryCreateView, UserPosts
 urlpatterns = [
     
-    path('posts/', PostListView.as_view()),
-    path('posts/<slug>/',  PostDetailView.as_view()),
+    path('list/', PostListView.as_view()),
+    path('list/<slug>/',  PostDetailView.as_view()),
     path('userposts/',UserPosts.as_view()),
     path('category/', CategoryCreateView.as_view()),
  
