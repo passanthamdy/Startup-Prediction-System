e826fee744d4f1d70048729c4ba942d7228aee95
Ahmadamr <ahmedamr18841@gmail.com>
2021-06-07 05:49:25 +0200

    fix add post

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -3,7 +3,7 @@ from .views import  PostDetailView, PostLike,AddPostDataset,CreatePostView, Post
 urlpatterns = [
     
     path('postcreate/', CreatePostView.as_view()), #to add post data like img and content bla
-    path('postcreate/createdataset/',AddPostDataset.as_view()),#to add dataset to be predicted
+    path('postcreate/<int:id>/',AddPostDataset.as_view()),#to add dataset to be predicted
     path('list/', PostListView.as_view() ), #to list all posts
     path('list/<int:id>/',  PostDetailView.as_view()), #to get post, update and delete 
     path('list/<int:id>/like/',  PostLike.as_view()),
